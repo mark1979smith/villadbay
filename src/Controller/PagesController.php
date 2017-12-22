a6d660e6c195a1be77b0588d7b16daace8ab30b8
Mark Smith <mark1979smith@googlemail.com>
2017-12-22 13:41:40 +0000

    Added layout and css

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -10,20 +10,67 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class PagesController
+class PagesController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function home()
     {
-        $number = mt_rand(0, 100);
+        $number = mt_rand(0, 1000);
 
-        return new Response(
-            '<html><body>Lucky number: '. $number .'</body></html>'
-        );
+        return $this->render('pages/home.html.twig', array(
+            'number' => $number,
+            'selectedNav' => 'home'
+        ));
+    }
+
+    /**
+     * @Route("/rooms", name="rooms")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function rooms()
+    {
+        $number = mt_rand(0, 1000);
+
+        return $this->render('pages/rooms.html.twig', array(
+            'number' => $number,
+            'selectedNav' => 'rooms'
+        ));
+    }
+
+    /**
+     * @Route("/about", name="about")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function about()
+    {
+        $number = mt_rand(0, 1000);
+
+        return $this->render('pages/about.html.twig', array(
+            'number' => $number,
+            'selectedNav' => 'about'
+        ));
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function contact()
+    {
+        $number = mt_rand(0, 1000);
+
+        return $this->render('pages/about.html.twig', array(
+            'number' => $number,
+            'selectedNav' => 'contact'
+        ));
     }
 }
