836f704f93250c95929f5f8b862e4bc601212a46
Mark Smith <marks1979smith@googlemail.com>
2018-01-15 12:33:12 +0000

    Added Admin Controller along with Admin layout file

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mark.smith
+ * Date: 15/01/2018
+ * Time: 12:12
+ */
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class AdminController
+ *
+ * @Route("/admin")
+ * @package App\Controller
+ */
+class AdminController extends Controller
+{
+    /**
+     * @Route("/", name="admin-home")
+     */
+    public function home()
+    {
+        return $this->render('admin/home.html.twig', array(
+
+        ));
+    }
+}
