f6cf51ab09b53dce26c32f80829884874dc6c6fd
Mark Smith <marks1979smith@googlemail.com>
2018-01-17 18:52:07 +0000

    tidy up

--- src/Form/Admin/Types/PageType.php
+++ src/Form/Admin/Types/PageType.php
@@ -16,7 +16,7 @@ use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class PageType  extends AbstractType
+class PageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
