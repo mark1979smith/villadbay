fba1b2babecff34a49504ab345039d95520618bc
Mark Smith <mark1979smith@googlemail.com>
2018-01-23 23:44:18 +1000

    AddedPanoramicImage types

---
+++ src/Form/Admin/Types/PanoramicImage.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mark.smith
+ * Date: 23/01/2018
+ * Time: 13:07
+ */
+
+namespace App\Form\Admin\Types;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PanoramicImage extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new \App\Form\DataTransformer\PanoramicImage());
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => \App\Entity\Page\PanoramicImage::class,
+        ));
+    }
+
+    public function getParent()
+    {
+        return ChoiceType::class;
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'PanoramicImageType';
+    }
+}
---
+++ src/Form/DataTransformer/PanoramicImage.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mark.smith
+ * Date: 23/01/2018
+ * Time: 13:08
+ */
+
+namespace App\Form\DataTransformer;
+
+
+use Symfony\Component\Form\DataTransformerInterface;
+
+class PanoramicImage implements DataTransformerInterface
+{
+    /**
+     * @param \App\Entity\Page\PanoramicImage|string|null $value
+     *
+     * @return string
+     */
+    public function transform($value)
+    {
+        if (null === $value || "" === $value) {
+            return '';
+        }
+
+        return $value->getPanoramicImage();
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return \App\Entity\Page\PageType
+     */
+    public function reverseTransform($value)
+    {
+        return (new \App\Entity\Page\PanoramicImage())->setPanoramicImage($value);
+    }
+}
