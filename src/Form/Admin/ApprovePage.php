39b40622981068ac2fc885dfa9a28f26cef85601
Mark Smith <marks1979smith@googlemail.com>
2018-02-22 12:10:18 +0000

    Added unversioned files as per last commit

---
+++ src/Form/Admin/ApprovePage.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mark.smith
+ * Date: 22/02/2018
+ * Time: 09:31
+ */
+
+namespace App\Form\Admin;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ApprovePage extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('slug', HiddenType::class)
+            ->add('approve', SubmitType::class, [
+                'label' => 'Yes',
+                'attr'  => [
+                    'class' => 'btn btn-success btn-lg',
+                ],
+            ])
+            ->add('decline', SubmitType::class, [
+                'label' => 'No',
+                'attr'  => [
+                    'class' => 'btn btn-danger btn-lg',
+                ],
+            ]);
+    }
+}
