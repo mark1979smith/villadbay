aca0913f05c6732908855690854214abb6dc639e
Mark Smith <hosting@marksmith.email>
2018-03-23 04:25:00 +1000

    [AUTO] Updates to composer installation

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -83,11 +83,11 @@ final class Versions
   'symfony/thanks' => 'v1.0.7@bade4992c46ed722162694b4af8d72f84402819a',
   'symfony/var-dumper' => 'v4.0.6@c7d89044ed6ed3b7d8b558d509cca0666b947e58',
   'symfony/web-profiler-bundle' => 'v4.0.6@eaaf46b1ccaf83c8c97efd8d081e03d9ff0f29de',
-  'symfony/polyfill-iconv' => '*@9cfbaacf2097dc93bfdb0af04a80888d24544038',
-  'symfony/polyfill-php71' => '*@9cfbaacf2097dc93bfdb0af04a80888d24544038',
-  'symfony/polyfill-php70' => '*@9cfbaacf2097dc93bfdb0af04a80888d24544038',
-  'symfony/polyfill-php56' => '*@9cfbaacf2097dc93bfdb0af04a80888d24544038',
-  '__root__' => 'dev-image-manager@9cfbaacf2097dc93bfdb0af04a80888d24544038',
+  'symfony/polyfill-iconv' => '*@058a50f8833442bf194231eed537a495fa6bc4bc',
+  'symfony/polyfill-php71' => '*@058a50f8833442bf194231eed537a495fa6bc4bc',
+  'symfony/polyfill-php70' => '*@058a50f8833442bf194231eed537a495fa6bc4bc',
+  'symfony/polyfill-php56' => '*@058a50f8833442bf194231eed537a495fa6bc4bc',
+  '__root__' => 'dev-image-manager@058a50f8833442bf194231eed537a495fa6bc4bc',
 );
 
     private function __construct()
