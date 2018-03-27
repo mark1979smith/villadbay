802906b5e79125b65c41f74314a1d4eada932ec5
Mark Smith <hosting@marksmith.email>
2018-03-27 20:53:45 +1000

    [AUTO] Updates to composer installation

--- src/Controller/Admin/ImagesController.php
+++ src/Controller/Admin/ImagesController.php
@@ -17,6 +17,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
@@ -35,6 +36,7 @@ class ImagesController extends Controller
      * @param \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface $authorizationChecker
      *
      * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
      */
     public function index(AuthorizationCheckerInterface $authorizationChecker)
     {
@@ -48,80 +50,105 @@ class ImagesController extends Controller
     }
 
     /**
-     * @Route("/create/{type}", name="admin-images-create", defaults={"type":""})
+     * @Route("/create/", name="admin-images-create")
      * @param \Symfony\Component\HttpFoundation\Request                                    $request
-     * @param \Symfony\Component\DependencyInjection\ContainerInterface                    $container
      * @param \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface $authorizationChecker
      *
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-     * @throws \ImagickException
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
      */
     public function create(
         Request $request,
-        ContainerInterface $container,
         AuthorizationCheckerInterface $authorizationChecker
     ) {
-        if ($request->get('type') === "") {
-            $form = $this->createForm(ImageTypesType::class, new Type());
-
-            $form->handleRequest($request);
-
-
-            if ($form->isSubmitted() && $form->isValid()) {
-                return $this->redirectToRoute('admin-images-create', ['type' => $form->get('value')->getNormData()]);
-            }
-        } else {
-            $imageType = strtolower($request->get('type'));
-            switch ($imageType) {
-                case 'background':
-                    $entity = new Type\Background();
-                    $form = $this->createFormBuilder($entity)
-                        ->add('file', FileType::class, [
-                            'label' => 'Choose an image to upload',
-                            'attr'  => [
-                                'description' => 'Requirements: 1,200px minimum width and 1,200px minimum height.',
-                            ],
-                        ]);
-                    break;
-                case 'carousel':
-                    $entity = new Type\Carousel();
-                    $form = $this->createFormBuilder($entity)
-                        ->add('file', FileType::class, [
-                            'label' => 'Choose an image to upload',
-                            'attr'  => [
-                                'description' => 'Requirements: Width: 1,110px. Height: 955px.',
-                            ],
-                        ]);
-                    break;
-                case 'panoramic':
-                    $entity = new Type\Panoramic();
-                    $form = $this->createFormBuilder($entity)
-                        ->add('file', FileType::class, [
-                            'label' => 'Choose an image to upload',
-                            'attr'  => [
-                                'description' => 'Requirements: Landscape images only with a minimum width of 1,200px and a minimum height of 900px.',
-                            ],
-                        ]);
-                    break;
-                default:
-                    throw new \LogicException('Illegal type detected');
-            }
-            $form = $form->add('upload', SubmitType::class)
-                ->getForm();
-
-            $form->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()) {
-                /** @var \Symfony\Component\HttpFoundation\File\UploadedFile $file */
-                $file = $entity->getFile();
-
-                $fileName = $this->generateUniqueFileName();
-                $fileNameExt = $file->guessExtension();
-
-                $this->sendImageToAWS($request->get('type'), $file, $fileName, $fileNameExt);
-            }
+        if (false === $authorizationChecker->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException('Unable to access this page!');
         }
 
+        $form = $this->createForm(ImageTypesType::class, new Type());
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->redirectToRoute('admin-images-create-type', ['type' => $form->get('value')->getNormData()]);
+        }
+
+
+        $twigData = array(
+            'selectedNav' => 'admin-images',
+            'form'        => $form->createView(),
+        );
+
+        return $this->render('admin/images.create.html.twig', $twigData);
+    }
+
+    /**
+     * @Route("/create/{type}/", name="admin-images-create-type", defaults={"type":""}, requirements={"type": "[a-z]+"})
+     * @param \Symfony\Component\HttpFoundation\Request                                    $request
+     *
+     * @param \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface $authorizationChecker
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \ImagickException
+     * @throws \Psr\Cache\InvalidArgumentException
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     */
+    public function createType(Request $request, AuthorizationCheckerInterface $authorizationChecker)
+    {
+        if (false === $authorizationChecker->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException('Unable to access this page!');
+        }
+
+        $imageType = strtolower($request->get('type'));
+        switch ($imageType) {
+            case 'background':
+                $entity = new Type\Background();
+                $form = $this->createFormBuilder($entity)
+                    ->add('file', FileType::class, [
+                        'label' => 'Choose an image to upload',
+                        'attr'  => [
+                            'description' => 'Requirements: 1,200px minimum width and 1,200px minimum height.',
+                        ],
+                    ]);
+                break;
+            case 'carousel':
+                $entity = new Type\Carousel();
+                $form = $this->createFormBuilder($entity)
+                    ->add('file', FileType::class, [
+                        'label' => 'Choose an image to upload',
+                        'attr'  => [
+                            'description' => 'Requirements: Width: 1,110px. Height: 955px.',
+                        ],
+                    ]);
+                break;
+            case 'panoramic':
+                $entity = new Type\Panoramic();
+                $form = $this->createFormBuilder($entity)
+                    ->add('file', FileType::class, [
+                        'label' => 'Choose an image to upload',
+                        'attr'  => [
+                            'description' => 'Requirements: Landscape images only with a minimum width of 1,200px and a minimum height of 900px.',
+                        ],
+                    ]);
+                break;
+            default:
+                throw new \LogicException('Illegal type detected');
+        }
+        $form = $form->add('upload', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var \Symfony\Component\HttpFoundation\File\UploadedFile $file */
+            $file = $entity->getFile();
+
+            $fileName = $this->generateUniqueFileName();
+            $fileNameExt = $file->guessExtension();
+
+            $this->sendImageToAWS($request->get('type'), $file, $fileName, $fileNameExt);
+        }
 
         $twigData = array(
             'selectedNav' => 'admin-images',
@@ -145,15 +172,18 @@ class ImagesController extends Controller
     }
 
     /**
-     * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
-     * @param string                                              $fileName Thumbnails will use this as a directory
-     *                                                                      followed by a child directory lg, md, sm,
-     *                                                                      xs
-     * @param string                                              $fileNameExt
+     * @param string       $imageType
+     * @param UploadedFile $file
+     * @param string       $fileName Thumbnails will use this as a directory followed by a child directory lg, md, sm,
+     *                               xs
+     * @param string       $fileNameExt
+     *
+     * @return void
      *
      * @throws \ImagickException
+     * @throws \Psr\Cache\InvalidArgumentException
      */
-    private function sendImageToAWS($imageType, $file, $fileName, $fileNameExt)
+    private function sendImageToAWS(string $imageType, UploadedFile $file, string $fileName, string $fileNameExt): void
     {
         switch ($imageType) {
             case 'panoramic':
@@ -242,7 +272,7 @@ class ImagesController extends Controller
 
         $s3Client->putObject([
             'Bucket' => $s3Service->getBucket(),
-            'Key'    => 'images/' . $dirPrefix . '/' . $fileName .'.' . $fileNameExt,
+            'Key'    => 'images/' . $dirPrefix . '/' . $fileName . '.' . $fileNameExt,
             'Body'   => $imagick->getImageBlob(),
             'ACL'    => 'public-read',
         ]);
@@ -252,7 +282,7 @@ class ImagesController extends Controller
                 $imagick->scaleImage($thumbnailSettings['width'], $thumbnailSettings['rows'], $thumbnailSettings['bestfit']);
                 $s3Client->putObject([
                     'Bucket' => $s3Service->getBucket(),
-                    'Key'    => 'images/' . $dirPrefix . '/' . $fileName . '--'. $responsiveSizeClass.'.' . $fileNameExt,
+                    'Key'    => 'images/' . $dirPrefix . '/' . $fileName . '--' . $responsiveSizeClass . '.' . $fileNameExt,
                     'Body'   => $imagick->getImageBlob(),
                     'ACL'    => 'public-read',
                 ]);
@@ -267,7 +297,7 @@ class ImagesController extends Controller
             'Bucket' => $s3Service->getBucket(),
         ]);
 
-        $cacheKey = 'aws.s3.listobjects.'.$s3Service->getBucket();
+        $cacheKey = 'aws.s3.listobjects.' . $s3Service->getBucket();
         $cacheItem = $redisClient->getItem($cacheKey);
         $cacheItem->set($response);
 
