c06b8f246f3f126d226366496275365a68477bda
Mark Smith <mark1979smith@googlemail.com>
2018-10-25 08:11:49 +0100

    Updated params.php to allow CLI cache:clear

--- config/packages/params.php
+++ config/packages/params.php
@@ -1,2 +1,6 @@
 <?php
-$container->setParameter('host', $_SERVER['HTTP_HOST']);
+if (array_key_exists('HTTP_HOST', $_SERVER)) {
+    $container->setParameter('host', $_SERVER['HTTP_HOST']);
+} else {
+    $container->setParameter('host', 'www.villadbay.com');
+}
