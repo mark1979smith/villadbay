ef78abf92698ad47b15708b578265aa77b9e7b56
Mark Smith <hosting@marksmith.email>
2018-11-17 14:04:04 +0000

    Added Configuration component with tests

--- spec/Component/ConfigurationSpec.php
+++ spec/Component/ConfigurationSpec.php
@@ -8,27 +8,65 @@ use App\Entity\ConfigGroup;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+/**
+ * Class ConfigurationSpec
+ *
+ * @package spec\App\Component
+ */
 class ConfigurationSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function let()
     {
         $this->beConstructedWith($this->getData());
+    }
 
+    function it_is_initializable()
+    {
         $this->shouldHaveType(Configuration::class);
     }
 
-    function it_outputs_one_record()
+    function it_should_have_3_entries()
     {
-        $this->beConstructedWith($this->getData());
+        $this->callOnWrappedObject('getData')->offsetGet('test-123')->shouldHaveCount(3);
+    }
 
-        $this->render()->shouldBe('test');
+    function it_should_be_iterator_when_no_slug_provided()
+    {
+        $this->getLatestRevision()->shouldHaveType('ArrayIterator');
+    }
+
+    function it_should_be_entity_when_slug_provided()
+    {
+        $this->getLatestRevision('test-123')->shouldHaveType('App\Entity\Config');
+    }
+
+    function it_should_contain_key_when_no_slug_provided()
+    {
+        $this->getLatestRevision()->shouldHaveKey('test-123');
+    }
+
+    function it_should_have_value_test2_when_slug_provided()
+    {
+        $this->getLatestRevision('test-123')->getValue()->shouldBe('test2');
+    }
+
+    function it_should_be_entity_value_when_no_slug_provided()
+    {
+        $this->getLatestRevision()->offsetGet('test-123')->shouldHaveType('App\Entity\Config');
+    }
+
+    function it_should_have_value_test2_only_when_no_slug_provided()
+    {
+        $this->getLatestRevision()->offsetGet('test-123')->getValue()->shouldBe('test2');
     }
 
     function getData()
     {
         $configGroup = (new ConfigGroup())->setName('Core')->setDescription('Test123');
         return [
-            (new Config())->setConfigGroup($configGroup)->setSlug('test-123')->setValue('test')
+            (new Config())->setConfigGroup($configGroup)->setSlug('test-123')->setCreated(new \DateTimeImmutable('2018-11-16 00:00:00'))->setValue('test')
+            ,(new Config())->setConfigGroup($configGroup)->setSlug('test-123')->setCreated(new \DateTimeImmutable('2018-11-16 02:00:00'))->setValue('test2')
+            ,(new Config())->setConfigGroup($configGroup)->setSlug('test-123')->setCreated(new \DateTimeImmutable('2018-11-16 01:00:00'))->setValue('test1')
         ];
     }
 }
--- src/Component/Configuration.php
+++ src/Component/Configuration.php
@@ -2,24 +2,68 @@
 
 namespace App\Component;
 
+use App\Entity\Config;
+
+/**
+ * Class Configuration
+ *
+ * @package App\Component
+ */
 class Configuration
 {
+    /** @var \ArrayIterator */
     protected $data;
+
     public function __construct(array $data)
     {
-        $this->data = $data;
+        $this->data = $this->transferDataToIterable($data);
     }
 
-    public function render()
+    public function getData(): \ArrayIterator
     {
-        $str = '';
+        return $this->data;
+    }
 
-        /** @var \App\Entity\Config $configRow */
-        foreach ($this->data as $configRow) {
-            $str .= $configRow->getValue();
+    public function getLatestRevision(?string $slug = null)
+    {
+        $revisions = $this->data;
+
+        if (!is_null($slug)) {
+            /** @var \ArrayIterator $revisions */
+            $revisions = $revisions->offsetGet($slug);
+
+            return reset($revisions);
+        } else {
+            $array = new \ArrayIterator();
+            foreach ($revisions as $slug => $revisionSet) {
+                $array->offsetSet($slug, reset($revisionSet));
+            }
+            return $array;
+        }
+    }
+
+    protected function transferDataToIterable(array $data): \ArrayIterator
+    {
+        $array = new \ArrayIterator();
+        // Group By Slug
+        /** @var Config $config */
+        foreach ($data as $config) {
+            if (!$array->offsetExists($config->getSlug())) {
+                $array->offsetSet($config->getSlug(), new \ArrayIterator());
+            }
+            $array->offsetGet($config->getSlug())->append($config);
         }
 
-        return $str;
-    }
+        /**
+         * @var string         $slug
+         * @var \ArrayIterator $revisions
+         */
+        foreach ($array as $slug => $revisions) {
+            $revisions->uasort(function ($a, $b) {
+                return ($a->getCreated() > $b->getCreated()) ? -1 : 1;
+            });
+        }
 
+        return $array;
+    }
 }
