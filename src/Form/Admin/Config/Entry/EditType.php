0a391fa9a3eb8282a9e1133fbcca6be3aee21037
Mark Smith <hosting@marksmith.email>
2018-11-18 20:02:15 +0000

    Added validation to value in edit config form and changed layout due to form validation

--- src/Form/Admin/Config/Entry/EditType.php
+++ src/Form/Admin/Config/Entry/EditType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 /**
  * Class CreateType
@@ -17,7 +18,12 @@ class EditType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('value', TextType::class)
+            ->add('value', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                ]
+            ])
             ->add('save', SubmitType::class, [
                 'label' => 'Save entry',
                 'attr'  => [
