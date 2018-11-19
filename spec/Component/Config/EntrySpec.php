bfdb28b91b6186c5c44a3e8eac9b6a5d7ee4269a
Mark Smith <mark1979smith@googlemail.com>
2018-11-19 08:21:50 +0000

    Updated test to match expected data and updated view to stop showing config which did not belong in group

--- spec/Component/Config/EntrySpec.php
+++ spec/Component/Config/EntrySpec.php
@@ -49,6 +49,11 @@ class EntrySpec extends ObjectBehavior
         $this->getLatestRevision('test-123')->getValue()->shouldBe('test2');
     }
 
+    function it_should_have_x_entities_when_no_slug_provided()
+    {
+        $this->getLatestRevision()->shouldHaveCount(4);
+    }
+
     function it_should_be_entity_value_when_no_slug_provided()
     {
         $this->getLatestRevision()->offsetGet('test-123')->shouldHaveType('App\Entity\Config');
@@ -61,11 +66,16 @@ class EntrySpec extends ObjectBehavior
 
     function getData()
     {
-        $configGroup = (new ConfigGroup())->setName('Core')->setDescription('Test123');
+        $configGroup1 = (new ConfigGroup())->setName('Core')->setDescription('Test123');
+        $configGroup2 = (new ConfigGroup())->setName('Core 1')->setDescription('Test124');
+        $configGroup3 = (new ConfigGroup())->setName('Core 2')->setDescription('');
         return [
-            (new Config())->setConfigGroup($configGroup)->setSlug('test-123')->setCreated(new \DateTimeImmutable('2018-11-16 00:00:00'))->setValue('test')
-            ,(new Config())->setConfigGroup($configGroup)->setSlug('test-123')->setCreated(new \DateTimeImmutable('2018-11-16 02:00:00'))->setValue('test2')
-            ,(new Config())->setConfigGroup($configGroup)->setSlug('test-123')->setCreated(new \DateTimeImmutable('2018-11-16 01:00:00'))->setValue('test1')
+            (new Config())->setConfigGroup($configGroup1)->setSlug('test-123')->setCreated(new \DateTimeImmutable('2018-11-16 00:00:00'))->setValue('test')
+            ,(new Config())->setConfigGroup($configGroup1)->setSlug('test-123')->setCreated(new \DateTimeImmutable('2018-11-16 02:00:00'))->setValue('test2')
+            ,(new Config())->setConfigGroup($configGroup1)->setSlug('test-123')->setCreated(new \DateTimeImmutable('2018-11-16 01:00:00'))->setValue('test1')
+            ,(new Config())->setConfigGroup($configGroup2)->setSlug('test-1234')->setCreated(new \DateTimeImmutable('2018-11-20 01:00:00'))->setValue('test3')
+            ,(new Config())->setConfigGroup($configGroup3)->setSlug('test-1235')->setCreated(new \DateTimeImmutable('2018-11-20 01:00:00'))->setValue('test3')
+            ,(new Config())->setConfigGroup($configGroup3)->setSlug('test-1236')->setCreated(new \DateTimeImmutable('2018-11-20 01:00:00'))->setValue('test3')
         ];
     }
 }
