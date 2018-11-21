b84d9c89bb6e58913549d56d3bb330f40fcef6f4
mark1979smith <mark1979smith@googlemail.com>
2018-11-21 09:24:31 +0000

    Updated db host and port

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #create or update db
-./waitforit.sh <DB_HOST>:<DP_PORT> -t 30
+./waitforit.sh db:3306 -t 30
 /usr/local/bin/php /var/www/bin/console doctrine:migrations:migrate
 
 # start apache
