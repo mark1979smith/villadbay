d359a7f5951abd1d359c2f074485a4d61d1d62fb
mark1979smith <mark1979smith@googlemail.com>
2018-11-21 09:31:03 +0000

    Added --no-interaction flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #create or update db
 ./waitforit.sh db:3306 -t 30
-/usr/local/bin/php /var/www/bin/console doctrine:migrations:migrate
+/usr/local/bin/php /var/www/bin/console doctrine:migrations:migrate --no-interaction
 
 # start apache
 apache2-foreground
